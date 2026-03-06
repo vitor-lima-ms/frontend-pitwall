c85498fc6ed0418cf7a5db4f2419b7417d671241
Vitor Lima <viitorlima2000@gmail.com>
2026-03-06 01:37:51 -0300

    feat: att ColorPaletteEnum

--- src/enums/ui/color-palette.enum.ts
+++ src/enums/ui/color-palette.enum.ts
@@ -3,19 +3,17 @@ export enum ColorPaletteEnum {
   /* Preto */
   BLACK = "#000000",
   /* Derivado de SECONDARY_COLOR */
-  DARK_SUBMENU_BACKGROUND = "#10234A", /* color-7 do Palette Generation Tool (Ant Design) */
+  DARK_SUBMENU_BACKGROUND = "#C2002D", /* color-7 do Palette Generation Tool (Ant Design) */
   /* Grafite da identidade visual */
   GRAPHITE = "#161616",
-  /* Azul claro da identidade visual */
-  LIGHT_BLUE = "#B6E1F8",
-  /* Verde da identidade visual */
-  PRIMARY_COLOR = "#A2E122", /* color-6 do Palette Generation Tool (Ant Design) */
+  
+  PRIMARY_COLOR = "#15151E", /* color-6 do Palette Generation Tool (Ant Design) */
   /* Derivado de PRIMARY_COLOR */
-  PRIMARY_COLOR_LIGHT = "#E9FFA1", /* color-3 do Palette Generation Tool (Ant Design) */
-  /* Azul da identidade visual */
-  SECONDARY_COLOR = "#1E3B6F", /* color-6 do Palette Generation Tool (Ant Design) */
+  PRIMARY_COLOR_LIGHT = "#414145", /* color-3 do Palette Generation Tool (Ant Design) */
+  
+  SECONDARY_COLOR = "#E8002D", /* color-6 do Palette Generation Tool (Ant Design) */
   /* Derivado de SECONDARY_COLOR */
-  SECONDARY_COLOR_LIGHT = "#718296", /* color-3 do Palette Generation Tool (Ant Design) */
+  SECONDARY_COLOR_LIGHT = "#ff7a88", /* color-3 do Palette Generation Tool (Ant Design) */
   /* Branco */
   WHITE = "#FFFFFF"
 }
