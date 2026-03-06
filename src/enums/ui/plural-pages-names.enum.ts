b8cf121d9c0e5f5e62c5195d4e21202dd4380312
Vitor Lima <viitorlima2000@gmail.com>
2026-03-06 01:27:53 -0300

    refactor: adjust enums to new project pages

--- src/enums/ui/plural-pages-names.enum.ts
+++ src/enums/ui/plural-pages-names.enum.ts
@@ -1,16 +1,4 @@
 /* PluralPagesNamesEnum */
 export enum PluralPagesNamesEnum {
-  CLASS_OR_CATEGORY_OF_HAZARD = "Classes ou categorias de perigo",
-  ENVIRONMENTAL_MATRIX = "Matrizes ambientais",
-  EPA = "Avaliador de resultados EPA 1313/1316",
-  GENERIC_PARAMETER = "Parâmetros genéricos",
-  IMPORT = "Importações",
-  LABORATORY = "Laboratórios",
-  MAP = "Mapas",
-  PUBLISHER = "Publicadores",
-  REGULATION = "Normas",
-  SAMPLING_POINT = "Pontos de coleta",
-  TOXIC_PARAMETER = "Parâmetros tóxicos",
-  UNIT = "Unidades",
-  UTILS = "Utilitários",
+  DRIVER = "Pilotos",
 }
--- src/enums/ui/singular-pages-names.enum.ts
+++ src/enums/ui/singular-pages-names.enum.ts
@@ -1,16 +1,4 @@
 /* SingularPagesNamesEnum */
 export enum SingularPagesNamesEnum {
-  CLASS_OR_CATEGORY_OF_HAZARD = "Classe ou categoria de perigo",
-  ENVIRONMENTAL_MATRIX = "Matriz ambiental",
-  GENERIC_PARAMETER = "Parâmetro genérico",
-  HOME = "Home",
-  IMPORT = "Importação",
-  LABORATORY = "Laboratório",
-  LABORATORY_ANALYSIS = "Análise laboratorial",
-  MAP = "Mapas",
-  PUBLISHER = "Publicador",
-  REGULATION = "Norma",
-  SAMPLING_POINT = "Ponto de coleta",
-  TOXIC_PARAMETER = "Parâmetro tóxico",
-  UNIT = "Unidade",
+  DRIVER = "Piloto",
 }
