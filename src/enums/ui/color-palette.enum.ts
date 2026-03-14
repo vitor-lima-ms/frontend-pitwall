479b88680a776ecc03a8dd0645acc5ab3d8fbc05
Vitor Lima <viitorlima2000@gmail.com>
2026-03-14 13:00:44 -0300

    refactor: change PRIMARY_COLOR with SECONDARY_COLOR

--- src/enums/ui/color-palette.enum.ts
+++ src/enums/ui/color-palette.enum.ts
@@ -7,13 +7,13 @@ export enum ColorPaletteEnum {
   /* Grafite da identidade visual */
   GRAPHITE = "#161616",
   
-  PRIMARY_COLOR = "#15151E", /* color-6 do Palette Generation Tool (Ant Design) */
+  PRIMARY_COLOR = "#E8002D", /* color-6 do Palette Generation Tool (Ant Design) */
   /* Derivado de PRIMARY_COLOR */
-  PRIMARY_COLOR_LIGHT = "#414145", /* color-3 do Palette Generation Tool (Ant Design) */
+  PRIMARY_COLOR_LIGHT = "#ff7a88", /* color-3 do Palette Generation Tool (Ant Design) */
   
-  SECONDARY_COLOR = "#E8002D", /* color-6 do Palette Generation Tool (Ant Design) */
+  SECONDARY_COLOR = "#15151E", /* color-6 do Palette Generation Tool (Ant Design) */
   /* Derivado de SECONDARY_COLOR */
-  SECONDARY_COLOR_LIGHT = "#ff7a88", /* color-3 do Palette Generation Tool (Ant Design) */
+  SECONDARY_COLOR_LIGHT = "#414145", /* color-3 do Palette Generation Tool (Ant Design) */
   /* Branco */
   WHITE = "#FFFFFF"
 }
