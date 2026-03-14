d8b2a4917ef77ce5ca3c949e37988bfaf3e775b7
Vitor Lima <viitorlima2000@gmail.com>
2026-03-13 22:47:47 -0300

    feat: create FindDriversData

---
+++ src/responses/drivers/find-drivers-data.response.ts
@@ -0,0 +1,24 @@
+/* FindDriversData */
+export default interface FindDriversData {
+  MRData: {
+    DriverTable: {
+      Drivers: {
+        code: string;
+        dateOfBirth: string;
+        driverId: string;
+        familyName: string;
+        givenName: string;
+        nationality: string;
+        permanentNumber: string;
+        url: string;
+      }[];
+      season: string;
+    };
+    limit: string;
+    offset: string;
+    series: string;
+    total: string;
+    url: string;
+    xmlns: string;
+  };
+}
