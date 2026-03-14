1c4ab813c3b3008401b0939f00f77db23afd82ea
Vitor Lima <viitorlima2000@gmail.com>
2026-03-14 13:53:27 -0300

    refactor: temporary home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,19 @@
 "use client";
+/* Enum imports */
+import { SingularPagesNamesEnum } from "@/enums/ui/singular-pages-names.enum";
+/* Other libraries imports */
+import { useTitle } from "ahooks";
 /* Home */
-const Home: React.FC = () => <>Hello, World!</>
+const Home: React.FC = () => {
+  /**
+   * Definindo o título.
+   */
+  useTitle(SingularPagesNamesEnum.HOME);
+  /**
+   *
+   */
+
+  return <>Hello, World!</>;
+};
 
 export default Home;
